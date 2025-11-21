46792e4fe23f6466a0b1325427df6bb751b6db31
Lanyard <40360728+Lanyard31@users.noreply.github.com>
2025-11-21 15:02:41 +0100

    Create Leaderboard.cs

---
+++ Renegatta/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> names;
+    [SerializeField] private List<TextMeshProUGUI> scores;
+
+    private string publicLeaderboardKey =
+        "5b974e95d72d3fca3e4d9d537afe4a904af2082e77cd3be835c21c48c2af34b4";
+
+    private void Start()
+    {
+        GetLeaderboard();
+    }
+
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            int loopLength = (msg.Length < names.Count ? msg.Length : names.Count);
+            for (int i = 0; i < loopLength; i++)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            LeaderboardCreator.ResetPlayer();
+            GetLeaderboard();
+        }));
+    }
+}
